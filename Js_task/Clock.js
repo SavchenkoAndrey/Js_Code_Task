e1b4acf6b7c01c101e5f3ef9aa1d5293232c1de0
PC-ANATOLIY\AyzekUorren <proganatoliy@gmail.com>
2017-05-07 20:05:11 +0300

    added new features

--- Js_task/Clock.js
+++ Js_task/Clock.js
@@ -5,7 +5,7 @@ function CreateNewClock() {
 class Clock {
     constructor() {
         this.time = 55511;
-        this.timeType = 24;
+        this.timeType = true;
     }
 
     increaseSec() {
@@ -33,19 +33,15 @@ class Clock {
         this.time = parseInt(NewTime);
     }
 
-    setTimeType(NewType) {
-        if (NewType >= 24) {
-            this.timeType = 24;
-        } else {
-            this.timeType = 12;
-        }
+    setTimeType() {
+            this.timeType = !this.timeType;
     }
 
     stateTime() {
-        return this.timeType === 24;
+        return this.timeType === true;
     }
 
-    ConvertToHourWichConvention(){
+    ConvertToHourWithConvention(){
         if (parseInt(this.time / 3600) > 12) {
             this.timeConvention = 'P.M.';
             return parseInt((this.time / 3600) - 12);
@@ -75,12 +71,12 @@ class Clock {
             return `Time now: ${this.convertToHour().toString()} : ${this.convertToMin().toString()} : ${this.convertToSec().toString()}`;
         }
         else {
-            return `Time now: ${this.ConvertToHourWichConvention().toString()} ${this.timeConvention} : ${this.convertToMin().toString()} : ${this.convertToSec().toString()}`;
+            return `Time now: ${this.ConvertToHourWithConvention().toString()} ${this.timeConvention} : ${this.convertToMin().toString()} : ${this.convertToSec().toString()}`;
         }
     }
 }
 
-var NewClock = CreateNewClock();
+let NewClock = CreateNewClock();
 
 console.log(NewClock.toString());
 
@@ -91,8 +87,189 @@ NewClock.increaseSec();
 NewClock.increaseSec();
 console.log(NewClock.toString());
 
-NewClock.setTimeType(12);
+NewClock.setTimeType();
 console.log(NewClock.toString());
 
 NewClock.setTime(69000);
 console.log(NewClock.toString());
+
+function createClock_V1_2() {
+    return new Clock_V1_2();
+}
+
+Number.prototype.zeroFix=function(l){
+    return '00000000'.concat(this).slice(-l||-2)
+};
+
+class Clock_V1_2 extends Clock {
+    constructor() {
+        super();
+        this.light = true;
+    }
+
+    increaseMin() {
+        this.time += 60;
+        this.maxTime();
+    }
+    decreaseMin() {
+        this.time -= 60;
+        this.minTime();
+    }
+
+    increaseHour() {
+        this.time += 3600;
+        this.maxTime();
+    }
+    decreaseHour() {
+        this.time -= 3600;
+        this.minTime();
+    }
+
+    maxTime() {
+        if (this.time > 86400) {
+            this.time = 86400;
+        }
+    }
+
+    minTime() {
+        if (this.time < 0) {
+            this.time = 0;
+        }
+    }
+
+    correctStartTime() {
+        if (this.time > 86400) {
+            this.time -= 86400;
+        }
+    }
+
+    resetTime() {
+        this.time = 0;
+    }
+
+    changeLedLight() {
+        this.light = !this.light;
+    }
+
+    toString() {
+        this.correctStartTime();
+
+        if (this.stateTime()) {
+            return `Time now: ${this.convertToHour().zeroFix()} : ${this.convertToMin().zeroFix()} : ${this.convertToSec().zeroFix()} Led: ${this.light.toString()}`;
+        }
+        else {
+            return `Time now: ${this.ConvertToHourWithConvention().zeroFix()} ${this.timeConvention} : ${this.convertToMin().zeroFix()} : ${this.convertToSec().zeroFix()} Led: ${this.light.toString()}`;
+        }
+    }
+}
+
+
+let ClockWithLed = createClock_V1_2();
+console.log('\nClock Version 2\n');
+console.log(ClockWithLed.toString());
+
+ClockWithLed.resetTime();
+ClockWithLed.changeLedLight();
+console.log(ClockWithLed.toString());
+
+ClockWithLed.increaseMin();
+console.log(ClockWithLed.toString());
+
+ClockWithLed.increaseHour();
+ClockWithLed.increaseHour();
+ClockWithLed.increaseHour();
+ClockWithLed.increaseHour();
+ClockWithLed.increaseHour();
+ClockWithLed.setTimeType();
+console.log(ClockWithLed.toString());
+
+ClockWithLed.decreaseHour();
+ClockWithLed.decreaseMin();
+ClockWithLed.decreaseMin();
+console.log(ClockWithLed.toString());
+
+function createClock_V1_3() {
+    return new Clock_V_1_3();
+}
+
+class Clock_V_1_3 extends Clock_V1_2 {
+    constructor() {
+        super();
+        this.timer = 0;
+        this.changer = true;
+        this.counter = 0;
+    }
+
+    increaseSecTimer() {
+        this.timer += 1;
+    }
+
+    convertToHour() {
+        if (this.changer) {
+            return parseInt(this.time / 3600);
+        } else {
+            return parseInt(this.timer / 3600);
+        }
+
+    }
+
+    convertToMin() {
+        if (this.changer) {
+            return parseInt((this.time - (this.convertToHour() * 3600)) / 60);
+        } else {
+            return parseInt((this.timer - (this.convertToHour() * 3600)) / 60);
+        }
+    }
+
+    convertToSec() {
+        if (this.changer) {
+            return parseInt((this.time - (this.convertToHour() * 3600)) - this.convertToMin() * 60);
+        } else {
+            return parseInt((this.timer - (this.convertToHour() * 3600)) - this.convertToMin() * 60);
+        }
+    }
+
+    DisplayTimer() {
+        return `Timer: ${this.convertToHour().zeroFix()} : ${this.convertToMin().zeroFix()} : ${this.convertToSec().zeroFix()}`;
+    }
+
+    SetTimerSec(NewValue) {
+        this.counter = parseInt(NewValue * 1000 + 1000);
+    }
+
+    StartTimer() {
+        let CurentClock = this;
+        CurentClock.changer = false;
+        let timerID = setInterval(function () {
+            CurentClock.increaseSecTimer();
+        },1000);
+        setTimeout(function () {
+            clearInterval(timerID);
+        },CurentClock.counter)
+    }
+
+    toString() {
+        this.correctStartTime();
+        this.changer = true;
+        if (this.stateTime()) {
+            return `Time now: ${this.convertToHour().zeroFix()} : ${this.convertToMin().zeroFix()} : ${this.convertToSec().zeroFix()} Led: ${this.light.toString()}`;
+        }
+        else {
+            return `Time now: ${this.ConvertToHourWithConvention().zeroFix()} ${this.timeConvention} : ${this.convertToMin().zeroFix()} : ${this.convertToSec().zeroFix()} Led: ${this.light.toString()}`;
+        }
+    }
+}
+
+let ClockWithTimer = createClock_V1_3();
+
+console.log('\n Clock version 3\n');
+console.log(ClockWithTimer.toString());
+
+ClockWithTimer.SetTimerSec(10);
+ClockWithTimer.StartTimer();
+let intervalNewTimer = setInterval(function () {
+    console.log(ClockWithTimer.DisplayTimer().toString());
+}, 1000);
+setTimeout(function () {
+    clearInterval(intervalNewTimer)
+}, ClockWithTimer.counter);
